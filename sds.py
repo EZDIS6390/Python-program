a79076990b20f09c93982813573e0b015c2aa0f2
EZDIS6390 <32789545+EZDIS6390@users.noreply.github.com>
2017-10-14 15:26:09 +0530

    Add files via upload
    This is a python program to find out odd numbers from the list created by the user..

---
+++ sds.py
@@ -0,0 +1,15 @@
+L=[]
+n=int(input('No. of numbers : '))
+print('Enter ',n,' numbers into the list:-')
+i=0
+while i<n:
+    x=int(input())
+    L.append(x)
+    i=i+1
+for j in range(n+1):
+    if j%2!=0:
+        print(j)
+    
+    
+    
+    
